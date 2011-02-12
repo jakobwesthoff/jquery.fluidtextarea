8306e9b0356ead31ef045bd278111702fe8d5aeb
Jakob Westhoff <jakob@westhoffswelt.de>
2011-02-12 01:35:16 +0100

    Implemented initial version of the fluidtextarea plugin

---
+++ src/jquery.fluidtextarea.js
@@ -0,0 +1,273 @@
+/**
+ * Copyright (c) 2011 Jakob Westhoff
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+(function( $, jQuery ) {
+    /**
+     * Representation of a cloned textarea.
+     *
+     * The clone will inherit all the visual styles of the textarea, except
+     * its height definition. This way it is possible to determine the height
+     * the text currently in the textareas buffer does occupy.
+     *
+     * @constructor
+     * @param jQueryObject source
+     */
+    function ClonedTextArea( source ) {
+        var height;
+
+        /**
+         * Source element used for the generation of the clone
+         * 
+         * @private
+         * @type jQueryObject
+         */
+        this.source_ = source;
+
+        /**
+         * Text content currently stored inside the source textarea
+         *
+         * @private
+         * @type string
+         */
+        this.sourceContent_ = source.val();
+
+        /**
+         * Height of the cloned textarea
+         *
+         * @private
+         * @type number
+         */
+        this.sourceHeight_ = source.height();
+
+        /**
+         * Cloned element
+         *
+         * @private
+         * @type jQueryObject
+         */
+        this.clone_ = this.createClone_( source );
+
+        this.monitorTextArea_( source );
+    }
+
+    /**
+     * Create a clone of the given textarea and return it
+     *
+     * The clone will be a div element encapsulated into a jquery set.
+     * It will inherit all the styles of the source element except it's height
+     * restricting properties.
+     * 
+     * @private
+     * @param jQueryObject source
+     * @returns jQueryObject
+     */
+    ClonedTextArea.prototype.createClone_ = function( source ) {
+        var clone = $( "<div />" );
+        this.transferStyles_( source, clone );
+
+        this.transferText_( source, clone );
+        
+        return clone;
+    };
+
+    /**
+     * Transfer all css properties relevant for the dimensions of textareas
+     * from one element to another effectively making their size identical
+     *
+     * Properties controlling the height of the area are not transfered.
+     *
+     * @private
+     * @param jQueryObject source
+     * @param jQueryObject target
+     */
+    ClonedTextArea.prototype.transferStyles_ = function( source, target ) {
+        $.each(
+            [
+                'border-bottom-style',
+                'border-bottom-width',
+                'border-left-style',
+                'border-left-width',
+                'border-right-style',
+                'border-right-width',
+                'border-top-style',
+                'border-top-width',
+                'direction',
+                'font-family',
+                'font-size',
+                'font-style',
+                'font-weight',
+                'letter-spacing',
+                'line-height',
+                'max-width',
+                'min-width',
+                'overflow-x',
+                'overflow-y',
+                'padding-bottom',
+                'padding-left',
+                'padding-right',
+                'padding-top',
+                'text-align',
+                'text-decoration',
+                'text-transform',
+                'width',
+                'word-spacing',
+                'word-wrap'
+            ],
+            function( index, property ) {
+                target.css(
+                    property,
+                    source.css( property )
+                );
+            }
+        );
+    };
+
+    /**
+     * Transfer the text content from the source textarea to the cloned target.
+     *
+     * This method takes care to fix some quirks: 
+     *   - An empty line at the end of the content is ignored and therefore
+     *     ignored.
+     *   - More then one whitespace in a row is ignored
+     *   - Newlines are ignored
+     *  
+     * @private
+     * @param jQueryObject source
+     * @param jQueryObject target
+     */
+    ClonedTextArea.prototype.transferText_ = function( source, target ) {
+        var content = source.val();
+
+        // Escape the content to be used as html later on
+        content = $("<div />").text( content ).html();
+
+        /**
+         * Empty newlines at the end are ignored. A space is appended to fix
+         * this
+         */
+        if ( content.charAt( content.length -1 ) === "\n" ) {
+            content += " ";
+        }
+
+        /**
+         * Multiple whitespaces in a row are ignored therefore all whitespaces
+         * are replaced with the &nbsp; entitiy.
+         */
+        content = content.replace( / /g, "&nbsp;" );
+    
+        /**
+         * Newlines are ignored in normal html therefore they are replaced with
+         * explicit <br /> tags
+         */
+        content = content.replace( /\n/g, "<br />" );
+
+        target.html( content );
+    }
+
+    /**
+     * Monitor a given textarea for content changes.
+     *
+     * If the size of the textarea changes due to modifications of the content
+     * a height-changed event is fired on the ClonedTextArea object. The event
+     * receives the newly calculated height.
+     * 
+     * @private
+     * @param jQueryObject area
+     */
+    ClonedTextArea.prototype.monitorTextArea_ = function( area ) {        
+        area.bind(
+            "keyup.clonedtextarea",
+            jQuery.proxy( function( e ) {
+                var newContent
+                  , newHeight;
+
+                newContent = area.val();
+                if ( newContent === this.sourceContent_ ) {
+                    // The content did not change. No new height calculation is
+                    // needed.
+                    return;
+                }
+
+                this.sourceContent_ = newContent;
+                this.transferText_( area, this.clone_ );
+                
+                newHeight = this.getHeight();
+                if ( newHeight === this.sourceHeight_ ) {
+                    // Height did not change. Nothing to do.
+                    return;
+                }
+
+                this.sourceHeight_ = newHeight;
+                $(this).trigger( "height-changed", [newHeight] );
+            }, this )
+        );
+    };
+
+    /**
+     * Calculate the height of the cloned textarea and return it
+     *
+     * @return number
+     */
+    ClonedTextArea.prototype.getHeight = function() {
+        var height;
+
+        // In order to calculate the height of the element it needs to be added
+        // to the document.
+        $("body").append( this.clone_ );
+
+        height = this.clone_.height();
+        
+        // Remove the element from the dom again
+        this.clone_.detach();
+
+        return height;
+    };
+
+    jQuery.fn.fluidtextarea = function() {
+        this.filter( "textarea" ).each( function() {
+            var target = $(this)
+              , clone;
+
+            if ( target.data( "fluidtextarea-clone" ) ) {
+                // Fluidtextarea is already initialized. Ignore.
+                return;
+            }
+
+            // Scrollbars should no longer be displayed for the fluidtextarea
+            target.css( "overflow", "hidden" );
+
+            clone = new ClonedTextArea( target );
+            target.data( "fluidtextarea-clone", clone );
+            
+            // Set the needed height initially
+            target.height( clone.getHeight() );
+
+            // Monitor height changes to adapt to them
+            $(clone).bind( 
+                "height-changed", 
+                function( e, height ) {
+                    target.height( height );
+                }
+            );
+        });
+    };
+})( jQuery, jQuery );
